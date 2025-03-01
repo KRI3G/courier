77107aa909ad3e88063d1b22423eba01589ce879
KRI3G <kevin.guerra.v@gmail.com>
2025-02-28 23:46:12 -0600

    Playing with signature_pad source code

--- html/index.php
+++ html/index.php
@@ -5,7 +5,51 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Courier</title>
 </head>
-<body>
-    <script src="https://cdn.jsdelivr.net/npm/signature_pad@4.1.7/dist/signature_pad.umd.min.js"></script>
+<body onselectstart="return false">
+  <!-- Stolen from  https://github.com/szimek/signature_pad
+        Thanks, Szymon! :D
+        (c) 2023 Szymon Nowak | Released under the MIT license-->
+  <span id="forkongithub">
+    <a href="https://github.com/szimek/signature_pad">Fork me on GitHub</a>
+  </span>
+
+  <div id="signature-pad" class="signature-pad">
+    <div id="canvas-wrapper" class="signature-pad--body">
+      <canvas style="touch-action: none; user-select: none;" width="664" height="292"></canvas>
+    </div>
+    <div class="signature-pad--footer">
+      <div class="description">Sign above</div>
+
+      <div class="signature-pad--actions">
+        <div class="column">
+          <button type="button" class="button clear" data-action="clear">Clear</button>
+          <button type="button" class="button" data-action="undo" title="Ctrl-Z">Undo</button>
+          <button type="button" class="button" data-action="redo" title="Ctrl-Y">Redo</button>
+          <br>
+          <button type="button" class="button" data-action="change-color">Change color</button>
+          <button type="button" class="button" data-action="change-width">Change width</button>
+          <button type="button" class="button" data-action="change-background-color">Change background color</button>
+
+        </div>
+        <div class="column">
+          <button type="button" class="button save" data-action="save-png">Save as PNG</button>
+          <button type="button" class="button save" data-action="save-jpg">Save as JPG</button>
+          <button type="button" class="button save" data-action="save-svg">Save as SVG</button>
+          <button type="button" class="button save" data-action="save-svg-with-background">Save as SVG with
+            background</button>
+        </div>
+      </div>
+
+      <div>
+        <button type="button" class="button" data-action="open-in-window">Open in Window</button>
+      </div>
+    </div>
+  </div>
+
+  <script src="js/signature_pad.umd.min.js"></script>
+  <script src="js/app.js"></script>
+
+
+
 </body>
 </html>
