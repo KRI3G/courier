ce15fe9782f40e22c1d6e4aaad35ae55bf23dcac
KRI3G <kevin.guerra.v@gmail.com>
2025-02-28 23:31:58 -0600

    Added basic HTML page

--- html/index.php
+++ html/index.php
@@ -1,1 +1,11 @@
-<?php phpinfo(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Courier</title>
+</head>
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/signature_pad@4.1.7/dist/signature_pad.umd.min.js"></script>
+</body>
+</html>
