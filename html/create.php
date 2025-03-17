cb325075a99e7852f3ee6a12a0bb3cf191683346
KRI3G <kevin.guerra.v@gmail.com>
2025-03-17 01:14:50 -0500

    Done

--- html/create.php
+++ html/create.php
@@ -1,7 +1,4 @@
 <?php
-    error_reporting(E_ALL);
-    ini_set('display_errors', 1);
-    
     // Sanitation function (Thanks Chat)
     function sanitize_input($data) {
         if (is_array($data)) {
